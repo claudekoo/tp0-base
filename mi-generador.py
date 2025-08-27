dc001343bb73efa16cb6d402e0ea90661613b5ff
claudekoo <hg.cld.koo@gmail.com>
2025-08-27 17:11:39 -0300

    mi-generador.py reads from docker-compose-dev.yaml to generate docker compose file

--- mi-generador.py
+++ mi-generador.py
@@ -1,45 +1,52 @@
 import sys
 
+SERVER = 0
+CLIENT = 1
+NETWORKS = 2
+
 def generate_compose_file(output_filename: str, num_clients: int):
-    lines = []
+    with open("docker-compose-dev.yaml", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    
+    server_lines = []
+    client_lines = []
+    networks_lines = []
+    section = SERVER
 
-    lines.append("name: tp0")
-
-    lines.append("services:")
-
-    lines.append("  server:")
-    lines.append("    container_name: server")
-    lines.append("    image: server:latest")
-    lines.append("    entrypoint: python3 /main.py")
-    lines.append("    environment:")
-    lines.append("      - PYTHONUNBUFFERED=1")
-    lines.append("      - LOGGING_LEVEL=DEBUG")
-    lines.append("    networks:")
-    lines.append("      - testing_net")
-
-    for i in range(1, num_clients + 1):
+    for line in lines:
+        stripped = line.strip()
+        if stripped.startswith("client1:"):
+            section = CLIENT
+            continue
+        
+        if section == CLIENT:
+            if not line.startswith("  "):
+                section = NETWORKS
+                networks_lines.append(line)
+                continue
+            else:
+                client_lines.append(line)
+                continue
+        if section == SERVER:
+            server_lines.append(line)
+        if section == NETWORKS:
+            networks_lines.append(line)
+    
+    clients_lines = []
+    for i in range(1, num_clients+1):
         client_name = f"client{i}"
-        lines.append(f"  {client_name}:")
-        lines.append(f"    container_name: {client_name}")
-        lines.append("    image: client:latest")
-        lines.append("    entrypoint: /client")
-        lines.append("    environment:")
-        lines.append(f"      - CLI_ID={i}")
-        lines.append("      - CLI_LOG_LEVEL=DEBUG")
-        lines.append("    networks:")
-        lines.append("      - testing_net")
-        lines.append("    depends_on:")
-        lines.append("      - server")
-
-    lines.append("networks:")
-    lines.append("  testing_net:")
-    lines.append("    ipam:")
-    lines.append("      driver: default")
-    lines.append("      config:")
-    lines.append("        - subnet: 172.25.125.0/24")
-
+        clients_lines.append(f"  {client_name}:\n")
+        
+        for line in client_lines:
+            if "container_name: client1" in line:
+                clients_lines.append(f"    container_name: {client_name}\n")
+            elif "CLI_ID=1" in line:
+                clients_lines.append(f"      - CLI_ID={i}\n")
+            else:
+                clients_lines.append(line)
+        
     with open(output_filename, "w", encoding="utf-8") as f:
-        f.write("\n".join(lines) + "\n")
+        f.writelines(server_lines + clients_lines + networks_lines)
 
 def main():
     output_filename = sys.argv[1]
